469acb05c8faacf3ade4e6e643d46ac41f022f71
Jim Veldhuis <jimvelduis90@gmail.com>
2012-01-07 14:19:36 +0100

    Initialization of project Sallly.

---
+++ Source/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Forevenue::Application.config.secret_token = 'd899ec7c88b1a59c995fe35b987230d8a83bee3b87a84a033c0ed7fca8fa12a1291e21031a575b0cfe96126b3d8d263c623516e5e1319c84923e927c4f9951ef'
---
+++ Source/config/routes.rb
@@ -0,0 +1,16 @@
+Forevenue::Application.routes.draw do
+  get "sessions/new"
+
+  resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
+
+  match '/signup', :to => 'users#new'
+  match '/signin', :to => 'sessions#new'
+  match 'signout', :to => 'sessions#destroy'
+  
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+    match '/welcome', :to => 'pages#welcome'
+  
+  root :to => 'pages#home'
+end
