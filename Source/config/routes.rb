c36dab346c333073d73976cf86d8a46e653bb09f
Jim Veldhuis <jim@jimv.nl>
2012-01-12 19:36:52 +0100

    Changed layout to default twitter bootstrap, changed database from Forevenue to Sallly, and implemented wire frames into ROR.
    To change the database from Forevenue to Sally follow the following
    steps:
    1) Create database names: "Sallly"
    2) On the command-line enter: "rake db:migrate"
    3) Remove database "Forevenue"

--- Source/config/routes.rb
+++ Source/config/routes.rb
@@ -7,10 +7,13 @@ Forevenue::Application.routes.draw do
   match '/signup', :to => 'users#new'
   match '/signin', :to => 'sessions#new'
   match 'signout', :to => 'sessions#destroy'
-  
+
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
-    match '/welcome', :to => 'pages#welcome'
-  
+  match '/welcome', :to => 'pages#welcome'
+  match '/activities', :to => 'pages#activities'
+  match '/average', :to => 'pages#average'
+  match '/goal', :to => 'pages#goal'
+
   root :to => 'pages#home'
 end
