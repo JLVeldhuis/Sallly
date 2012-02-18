9809a5d3618cd17132a1c8d9b9b2404ad256037f
Jim Veldhuis <jim@jimv.nl>
2012-02-18 11:54:23 +0100

    Heartbeat update, Money pile chart added

--- Source/public/assets/javascript/heartrate.js
+++ Source/public/assets/javascript/heartrate.js
@@ -1,14 +1,9 @@
 function HeartRate(options) {
     this.context          = options && options.context || false;
-    this.maxFPS           = 100;
     this.path             = options && options.path || "./heartrate";
-    this.frameRate        = 0;
     this.frameCount       = 0;
     this.drawCount        = 0;
-    this.framesPerSecond  = 0;
-    this.timeline         = new Array(16);
-    this.lastUpdate       = (new Date()).getTime() / 1000;
-    this.updated          = false;
+    this.timeline         = new Array(32);
     
     if (this.context) {
         // Background template
@@ -18,19 +13,35 @@ function HeartRate(options) {
         this.timeline[0] = 50;
         this.timeline[1] = 50;
         this.timeline[2] = 50;
-        this.timeline[3] = 45;
-        this.timeline[4] = 60;
-        this.timeline[5] = 35;
-        this.timeline[6] = 100;
-        this.timeline[7] = 0;
-        this.timeline[8] = 50;
-        this.timeline[9] = 50;
+        this.timeline[3] = 50;
+        this.timeline[4] = 50;
+        this.timeline[5] = 45;
+        this.timeline[6] = 60;
+        this.timeline[7] = 35;
+        this.timeline[8] = 100;
+        this.timeline[9] = 0;
         this.timeline[10] = 50;
-        this.timeline[11] = 45;
-        this.timeline[12] = 60;
-        this.timeline[13] = 35;
-        this.timeline[14] = 100;
-        this.timeline[15] = 0;
+        this.timeline[11] = 50;
+        this.timeline[12] = 50;
+        this.timeline[13] = 50;
+        this.timeline[14] = 50;
+        this.timeline[15] = 45;
+        this.timeline[16] = 60;
+        this.timeline[17] = 35;
+        this.timeline[18] = 100;
+        this.timeline[19] = 0;
+        this.timeline[20] = 50;
+        this.timeline[21] = 50;
+        this.timeline[22] = 50;
+        this.timeline[23] = 50;
+        this.timeline[24] = 50;
+        this.timeline[25] = 50;
+        this.timeline[26] = 50;
+        this.timeline[27] = 50;
+        this.timeline[28] = 50;
+        this.timeline[29] = 50;
+        this.timeline[30] = 50;
+        this.timeline[31] = 50;
         
         // Sprite strip
         this.sprites        = new Image();
@@ -44,29 +55,21 @@ function HeartRate(options) {
                                return function() { heartRate.spritesReady = true; };
                                }(this));
         
-        // Text color
-        this.textColor      = "rgba(100, 64, 64, 1)";
-        
         // Graph plot color
         this.plotColor     = "rgba(200, 64, 80, 1)";
     }
 }
 
-HeartRate.prototype.calculate = function() {
-    var frameTime = (new Date()).getTime();
-    
-    if (frameTime - this.lastUpdate > 1000) {
-        this.lastUpdate = frameTime;
-        var tmp = this.timeline.shift();
-        this.timeline.push(tmp);    
-    }
-    
+HeartRate.prototype.calculate = function() {    
     this.frameCount++;
+    
+    if (this.frameCount == 31) {
+        this.frameCount = 0;
+    }
 };
 
 // This method calculates FPS and Renders the monitor
 HeartRate.prototype.monitor = function() {
-    // only calculate if calculate has not already been called somewhere else
     this.calculate();
     
     var ctx = this.context;
@@ -86,14 +89,14 @@ HeartRate.prototype.monitor = function() {
         // Line graph
         ctx.strokeStyle = this.plotColor;
         
-        for (var i = 0, len = this.timeline.length - 1; i < len; i++) {
+        for (var i = 0, len = this.frameCount; i < len; i++) {
             var plot1 = 25 - this.timeline[i] / 100 * 20, 
             plot2 = 25 - this.timeline[i+1] / 100 * 20;
             
             if (!isNaN(plot1)) { 
                 ctx.beginPath();
-                ctx.moveTo(i * plotScale + 64, plot1);
-                ctx.lineTo((i+1) * plotScale + 64, plot2);
+                ctx.moveTo(i * plotScale + 35, plot1);
+                ctx.lineTo((i+1) * plotScale + 35, plot2);
                 ctx.stroke();
                 ctx.closePath();
             }
