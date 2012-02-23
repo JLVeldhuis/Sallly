da20ac94c574c52f2bf2b029478a8368c520a192
Jim Veldhuis <jim@jimv.nl>
2012-02-23 17:37:41 +0100

    Introduced calendar events

--- Source/app/models/user.rb
+++ Source/app/models/user.rb
@@ -16,6 +16,7 @@ class User < ActiveRecord::Base
   before_save :encrypt_password
     
     has_one :setting
+    has_many :calendar_events
   
   def has_password?(submitted_password)
     encrypted_password == encrypt(submitted_password)
