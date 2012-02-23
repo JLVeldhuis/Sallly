3a97d7c7c0dec681e8bd2a53dc0387fe583a9f2a
Jim Veldhuis <jim@jimv.nl>
2012-02-23 19:39:57 +0100

    Changed settings creation field

--- Source/db/migrate/20120218123219_create_settings.rb
+++ Source/db/migrate/20120218123219_create_settings.rb
@@ -1,8 +1,22 @@
 class CreateSettings < ActiveRecord::Migration
-  def change
+  def self.up
     create_table :settings do |t|
-
+        t.column :goal_revenue, :integer
+        t.column :goal_start, :date
+        t.column :goal_end, :date
+        t.column :average_revenue, :integer
+        t.column :average_start, :date
+        t.column :average_end, :date
+        t.column :activity_level, :integer
+        t.column :activity_calls, :integer
+        t.column :activity_visits, :integer
+        t.column :activity_quotes, :integer
+        t.column :user_id, :integer
       t.timestamps
     end
+  end
+
+  def self.down
+      drop_table :settings
   end
 end
