ffec83dd32e5e439701ba1740e6ff73c6505c06e
Deepak Sharma <geekdeepak@gmail.com>
2012-04-19 14:50:55 +0530

    calendar live events

--- Source/app/helpers/pages_helper.rb
+++ Source/app/helpers/pages_helper.rb
@@ -1,2 +1,18 @@
 module PagesHelper
+  def print_events
+    events_js = ""
+    current_user.events.each do |event|
+      if event.title != nil && event.date_from != nil
+        events_js += "{
+                        title: '#{event.title}',
+                        start: new Date(#{event.date_from.strftime('%Y, %m-1, %d')})"
+        if event.date_to !=nil
+          events_js+=",end: new Date(#{event.date_to.strftime('%Y, %m-1, %d')})"
+        end
+        events_js+="},"
+      end
+    end
+    events_js = events_js.chop
+    return events_js
+  end
 end
