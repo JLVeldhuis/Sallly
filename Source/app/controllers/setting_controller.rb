f6d8d0d167d356765ee20b5e3bdd884057fb7dbb
Raj Kumar Goel <goyalkrraj@gmail.com>
2012-04-30 10:53:34 +0530

    added activity calls as events afetr event update

--- Source/app/controllers/setting_controller.rb
+++ Source/app/controllers/setting_controller.rb
@@ -1,20 +1,21 @@
 class SettingController < ApplicationController
-    def new
-      @setting = Setting.new
-    end
-    
-    def update
-      @setting = Setting.find(current_user.setting.id)
-      if @setting.update_attributes(params[:setting])
-        flash[:success] = "Setting updated."
-        
-        if params[:target].nil?
-          @redirect = "settings"
-        else
-          @redirect = params[:target];
-        end
-            
-        redirect_to "/#{@redirect}"
+  def new
+    @setting = Setting.new
+  end
+  
+  def update
+    # @setting = Setting.find(current_user.setting.id)
+    @setting  = current_user.setting
+    if @setting.update_attributes(params[:setting])
+      flash[:success] = "Setting updated."
+      
+      if params[:target].nil?
+        @redirect = "settings"
+      else
+        @redirect = params[:target];
       end
+          
+      redirect_to "/#{@redirect}"
     end
+  end
 end
--- Source/app/models/setting.rb
+++ Source/app/models/setting.rb
@@ -2,6 +2,35 @@ class Setting < ActiveRecord::Base
   belongs_to :user
   
   WORKING_HOURS_A_DAY = 8
+  CALL_GAP_IN_MINUTES = 5
+  
+  after_save  :populate_events
+  
+  # populate events like cold calls, cold visits, cold quotes
+  # Calls: depends upon number of cold_calls and goal_start and goal_end
+  def populate_events 
+    if activity_calls > 0
+      goal_period_in_mins = ((goal_end - goal_start) / 1.minute).floor
+      goal_period_in_mins = (WORKING_HOURS_A_DAY.hours/1.minute).floor unless goal_period_in_mins > 0
+    
+      call_period         = (goal_period_in_mins / activity_calls).floor
+    
+      eStart = self.goal_start
+      eEnd   = self.goal_start + call_period.minutes
+      
+      activity_calls.times do |i|
+        self.user.events << Event.new({
+                                        :title      => "Via setting at #{eStart} to #{eEnd}",
+                                        :eventtype  => "Others",
+                                        :date_from  => eStart,
+                                        :date_to    => eEnd
+                                     })
+        eStart = eEnd + CALL_GAP_IN_MINUTES.minutes
+        eEnd   += CALL_GAP_IN_MINUTES.minutes + call_period.minutes
+      end
+      self.user.save
+    end
+  end
   
   # populating methods on the basis of input values of setting
   
@@ -62,25 +91,25 @@ class Setting < ActiveRecord::Base
   # average_working_hours_per_week
   # WORKING_HOURS_A_DAY * average_workdays_per_week
   def average_working_hours_per_week
-    WORKING_HOURS_A_DAY * average_workdays_per_week
+    WORKING_HOURS_A_DAY * average_workdays_a_week
   end
   
-  # cold call
+  # cold calls
   # average_working_hours_per_week / calls_per_week
-  def cold_call
-    average_working_hours_per_week/calls_per_week
+  def cold_calls
+    (average_working_hours_per_week.to_f/calls_per_week).ceil
   end
   
-  # cold visit
+  # cold visits
   # average_working_hours_per_week / visits_per_week
-  def cold_visit
-    average_working_hours_per_week/visits_per_week
+  def cold_visits
+    (average_working_hours_per_week.to_f/visits_per_week).ceil
   end
   
-  # cold quote
+  # cold quotes
   # average_working_hours_per_week / quotes_per_week
-  def cold_quote
-    average_working_hours_per_week/quotes_per_week
+  def cold_quotes
+    (average_working_hours_per_week.to_f/quotes_per_week).ceil
   end
   
   # value per call
