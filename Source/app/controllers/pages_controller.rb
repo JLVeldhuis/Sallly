d8f56dc2277465056aafe57d42e003b012d12690
Jim Veldhuis <jimveldhuis90@gmail.com>
2012-06-05 21:26:26 +0200

    Removed unnessacery code

--- Source/app/controllers/pages_controller.rb
+++ Source/app/controllers/pages_controller.rb
@@ -2,10 +2,6 @@ class PagesController < ApplicationController
   skip_filter :authenticate_user!, :only => [:home]
 
   def status
-    @event  = current_user.events.build() if signed_in?
-    @month  = 02
-    @year   = 2012
-    @offset = Time.local(2012, 03, 01).wday
   end
   
   def get_events
