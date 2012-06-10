9501b927d4d4107006c9f892a648b6d929518349
Raj Kumar Goel <goyalkrraj@gmail.com>
2012-06-10 22:45:12 +0530

    added the limit for the activities

--- Source/app/models/event.rb
+++ Source/app/models/event.rb
@@ -2,6 +2,8 @@ class Event < ActiveRecord::Base
   belongs_to :user
   belongs_to :lead
   
+  MAX_EVENT_COUNT = 10
+  
   Eventtype = {
                 "Cold Calls" => 1,
                 "Visits"     => 2,
@@ -14,4 +16,11 @@ class Event < ActiveRecord::Base
   
   # all inactive events from taskmanager 
   scope :via_taskmanager_and_inactive, where(:source => true, :is_active => false)
+  
+  before_validation :check_event_limit,
+                    :on => :create
+  
+  def check_event_limit
+    self.errors.add(:user_id, "User has reached his activity limit of #{MAX_EVENT_COUNT}") if self.user.events.count >= 10
+  end
 end
