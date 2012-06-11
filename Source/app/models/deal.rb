37fe44f9f6b77388cf90a1202516ad2dd472e4af
Raj Kumar Goel <goyalkrraj@gmail.com>
2012-06-11 21:27:40 +0530

    re-arranged the settings page

--- Source/app/models/deal.rb
+++ Source/app/models/deal.rb
@@ -1,3 +1,19 @@
 class Deal < ActiveRecord::Base
   belongs_to :event
+  
+  DEALTYPE = {
+               "Fixed bid"    => "fixed",
+               "Per hour"     => "hour",
+               "Per month"    => "month",
+               "Per year"     => "year"
+             }
+             
+  CURRENCY = {
+                "$ - USD"   => "USD",
+                "# - POUND" => "POUND"
+             }
+             
+  CATEGORY = {
+                "None" => "NONE"
+             }
 end
