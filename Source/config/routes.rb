394db5bcb6ab31f72b6aaafdb64ed2425f446251
Jim Veldhuis <jim@jimv.nl>
2012-06-19 19:46:27 +0200

    Look&Feel update

--- Source/config/routes.rb
+++ Source/config/routes.rb
@@ -1,17 +1,17 @@
-Forevenue::Application.routes.draw do  
+Forevenue::Application.routes.draw do
   devise_scope :user do
     get "sign_in", :to => "devise/sessions#new"
     get '/users/auth/:provider' => 'users/omniauth_callbacks#passthru'
   end
-  
-  devise_for :users, 
+
+  devise_for :users,
              :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :sessions => "sessions"}
 
   match '/registrations' => 'registrations#email', :via => :get,  :as => "email"
   match '/registrations' => 'registrations#email', :via => :post
-  
+
   resources :deals, :only => [:create, :update]
-  
+
   resources :events, :setting, :leads
 
   match '/contact',     :to => 'pages#contact'
@@ -25,11 +25,11 @@ Forevenue::Application.routes.draw do
   match '/status',      :to => 'pages#status', :as => "status"
   match '/video',       :to => 'pages#video'
 
-  match '/events/new',              :to => 'events#new'  
+  match '/events/new',              :to => 'events#new'
   match '/events/:id/edit',         :to => 'events#edit'
   match '/get_events.json',         :to => 'pages#get_events'
   match '/get_upcoming_event.json', :to => 'pages#get_upcoming_event'
-  
+
   match '/events/accept/:id', :to => 'events#accept'
   match '/events/end/:id', :to => 'events#end'
   match 'refresh/leads' , :to => 'leads#refresh', :as => 'refresh_leads'
